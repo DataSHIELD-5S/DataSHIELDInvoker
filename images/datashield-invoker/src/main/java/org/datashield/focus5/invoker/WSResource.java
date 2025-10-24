b07acf3c6a0420df864f6f03e4f4059e55329dc1
Stuart Wheater <stuart.wheater@arjuna.com>
2025-10-24 16:42:17 +0100

    Improvement in Invoker

--- images/datashield-invoker/src/main/java/org/datashield/focus5/invoker/WSResource.java
+++ images/datashield-invoker/src/main/java/org/datashield/focus5/invoker/WSResource.java
@@ -35,6 +35,9 @@ public class WSResource
         logger.infof("          contentType    = %s", contentType);
         logger.infof("          contentLength  = %s", contentLength);
 
+        
+
+
         return ResponseBuilder.ok("", MediaType.APPLICATION_JSON /* MediaType.TEXT_PLAIN */)
                               .header("x-opal-version", "5.2.1")
                               .build();
@@ -48,84 +51,5 @@ public class WSResource
 
         return ResponseBuilder.ok("", MediaType.APPLICATION_JSON)
                               .build();
-    }
-
-    @GET
-    public String getRoot()
-    {
-        logger.info("Get Root");
-
-        return "Get Root";
-    }
-
-    @GET
-    @Path("/{path}")
-    public String getPath(String path)
-    {
-        logger.info("Get Path \"" + path + "\"");
-
-        return "Get Path \"" + path + "\"";
-    }
-
-    @GET
-    @Path("/{p1}/{p2}")
-    public String getCall2(String p1, String p2)
-    {
-        logger.info("Call-2: 'Get' " + p1 + " " + p2);
-
-        return "Call-2: 'Get' " + p1 + " " + p2;
-    }
-
-    @GET
-    @Path("/{p1}/{p2}/{p3}")
-    public String getCall3(String p1, String p2, String p3)
-    {
-        logger.info("Call-3: 'Get' " + p1 + " " + p2 + " " + p3);
-
-        return "Call-3: 'Get' " + p1 + " " + p2 + " " + p3;
-    }
-
-    @GET
-    @Path("/{p1}/{p2}/{p3}/{p4}")
-    public String getCall4(String p1, String p2, String p3, String p4)
-    {
-        logger.info("Call-4: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4);
-
-        return "Call-4: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4;
-    }
-
-    @GET
-    @Path("/{p1}/{p2}/{p3}/{p4}/{p5}")
-    public String getCall5(String p1, String p2, String p3, String p4, String p5)
-    {
-        logger.info("Call-5: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4 + " " + p5);
-
-        return "Call-5: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4 + " " + p5;
-    }
-
-    @GET
-    @Path("/{p1}/{p2}/{p3}/{p4}/{p5}/{p6}")
-    public String getCall6(String p1, String p2, String p3, String p4, String p5, String p6)
-    {
-        logger.info("Call-6: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4 + " " + p5 + " " + p6);
-
-        return "Call-6: 'Get' " + p1 + " " + p2 + " " + p3 + " " + p4 + " " + p5 + " " + p6;
-    }
-
-    @POST
-    public String postRoot()
-    {
-        logger.info("Post Root");
-
-        return "Post Root";
-    }
-
-    @POST
-    @Path("/{path}")
-    public String postPath(String path)
-    {
-        logger.info("Post Path \"" + path + "\"");
-
-        return "Post Path \"" + path + "\"";
     }
 }
